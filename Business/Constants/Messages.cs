cd38cae94cca96c80657b5515200a9be1ff826d2
Büşra Uylaş <busrauylas01@gmail.com>
2021-02-17 13:34:48 +0300

    11. Gün  1. Ödev

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,33 +6,33 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string ColorAdded = "\nColor added!\n";
-        public static string ColorUpdated = "\nColor updated!\n";
-        public static string ColorDeleted = "\nColor deleted!\n";
+        public static string ColorAdded = "Color added!";
+        public static string ColorUpdated = "Color updated!";
+        public static string ColorDeleted = "Color deleted!";
 
-        public static string CarAdded = "\nNew car added! ";
-        public static string CarUpdated = "\nInformation about car updated!\n";
-        public static string CarCouldNotUpdated = "\nInformation about car could not be updated.Daily Price must be greater than 0.\n";
-        public static string CarDeleted = "\nCar deleted!\n";
-        public static string InvalidDailyPrice = "\nCar could not be added. Daily Price must be greater than 0.\n";
-        public static string InvalidCarName = "\nCar could not be added. The lenght of the car name must be greater or equal to .\n";
+        public static string CarAdded = "New car added! ";
+        public static string CarUpdated = "Information about car updated!";
+        public static string CarCouldNotUpdated = "Information about car could not be updated.Daily Price must be greater than 0.";
+        public static string CarDeleted = "Car deleted!";
+        public static string InvalidDailyPrice = "Car could not be added. Daily Price must be greater than 0.";
+        public static string InvalidCarName = "Car could not be added. The lenght of the car name must be greater or equal to .";
         
-        public static string BrandAdded = "\nBrand added!\n";
-        public static string BrandUpdated = "\nBrand updated!\n";
-        public static string BrandDeleted = "\nBrand deleted!\n";
+        public static string BrandAdded = "Brand added!";
+        public static string BrandUpdated = "Brand updated!";
+        public static string BrandDeleted = "Brand deleted!";
 
-        public static string UserAdded = "\nUser added!\n";
-        public static string UserUpdated = "\nUser updated!\n";
-        public static string UserDeleted = "\nUser deleted!\n";
+        public static string UserAdded = "User added!";
+        public static string UserUpdated = "User updated!";
+        public static string UserDeleted = "User deleted!";
 
-        public static string CustomerAdded = "\nCustomer added!\n";
-        public static string CustomerUpdated = "\nCustomer updated!\n";
-        public static string CustomerDeleted = "\nCustomer deleted!\n";
+        public static string CustomerAdded = "Customer added!";
+        public static string CustomerUpdated = "Customer updated!";
+        public static string CustomerDeleted = "Customer deleted!";
 
-        public static string RentalCarAdded = "\nRental car added!\n";
-        public static string RentalCarUpdated = "\nRental car updated!\n";
-        public static string RentalCarDeleted = "\nRental car deleted!\n";
-        public static string RentalCarCouldNotAdded = "\nRental car could not be added.\n";
-        public static string RentalCarReturnDateError = "\nRental car could not be added.\n";
+        public static string RentalCarAdded = "Rental car added!";
+        public static string RentalCarUpdated = "Rental car updated!";
+        public static string RentalCarDeleted = "Rental car deleted!";
+        public static string RentalCarCouldNotAdded = "Rental car could not be added.";
+        public static string RentalCarReturnDateError = "Rental car could not be added.";
     }
 }
--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -8,9 +8,7 @@ namespace Entities.DTOs
    public  class CarDetailDto:IDto
     {
         public int Id { get; set; }
-        public int BrandId { get; set; }
         public string BrandName { get; set; }
-        public int ColorId { get; set; }
         public string ColorName { get; set; }
         public int ModelYear { get; set; }
         public double DailyPrice { get; set; }
